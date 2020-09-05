89930246da2a52b3b15b6a8113cd964e12c461fe
Diego A. Zelaya <30903842+Diego-Zelaya@users.noreply.github.com>
2020-09-04 19:08:01 -0700

    Add files via upload

---
+++ CS583F18_2D_Game_ZelayaD_MyJourney/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //adds functionality to play MyJourney Button
+    public void PlayMyJourney ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+
+
+}//end of class MainMenu
+ 
---
+++ CS583F18_2D_Game_ZelayaD_MyJourney/Assets/MySound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class MySound {
+
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0.1f, 4f)]
+    public float pitch;
+    [Range(0f, 2f)]
+    public float volume;
+
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
+ 
